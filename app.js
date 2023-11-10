6d2eedea898da18edfbb896c97e52ea8afa7d98f
pavankumar1-G <pavanpa1paone21@gmail.com>
2023-11-10 06:11:47 +0000

    added changes

--- app.js
+++ app.js
@@ -29,6 +29,15 @@ intializingDBAndServer();
 
 //Get players by using API:
 
+const convertDbObjectToResponseObject = (DBObject) => {
+  return {
+    playerId: DBObject.player_id,
+    playerName: DBObject.player_name,
+    jerseyNumber: DBObject.jersey_number,
+    role: DBObject.role,
+  };
+};
+
 app.get("/players/", async (request, response) => {
   const getPlayersListWithQuery = `
     SELECT *
@@ -36,7 +45,11 @@ app.get("/players/", async (request, response) => {
     cricket_team;
     `;
   const playersListArray = await database.all(getPlayersListWithQuery);
-  response.send(playersListArray);
+  response.send(
+    playersListArray.map((each_player) =>
+      convertDbObjectToResponseObject(each_player)
+    )
+  );
 });
 
 // Add player by using API:
@@ -70,8 +83,11 @@ app.get("/players/:playerId/", async (request, response) => {
     player_id = ${playerId};
     `;
   const playerDetails = await database.get(getPlayerWithQuery);
-  response.send(playerDetails);
+
+  response.send(convertDbObjectToResponseObject(playerDetails));
 });
+
+//Update player details by using API:
 
 app.put("/players/:playerId/", async (request, response) => {
   const { playerId } = request.params;
@@ -81,12 +97,30 @@ app.put("/players/:playerId/", async (request, response) => {
     UPDATE
       cricket_team
     SET
-      player_name = ${playerName},
-      jerseyNumber = ${jerseyNumber},
-      role = ${role}
+      player_name = "${playerName}",
+      jersey_number = ${jerseyNumber},
+      role = "${role}"
     WHERE
       player_id = ${playerId};
     `;
   await database.run(updatePlayerDetailsWithQuery);
   response.send("Player Details Updated");
 });
+
+// Delete player details by using API:
+
+app.delete("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const deletePlayerDetailsWithSqlQuery = `
+    DELETE
+    FROM
+    cricket_team
+    WHERE
+    player_id = ${playerId};
+
+    `;
+  await database.run(deletePlayerDetailsWithSqlQuery);
+  response.send("Player Removed");
+});
+
+module.exports = app;
